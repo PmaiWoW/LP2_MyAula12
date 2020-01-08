b1b58656e0b549a1a411b19d1f23770642d78034
PmaiWoW <PmaiWoW@Gmail.com>
2020-01-08 17:45:14 +0000

    Update exercise 1 to to fit exercise 3 criteria

---
+++ Exercício1/Exercício1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer enemySprite;
+    [SerializeField] private EnemyModel enemyModel;
+
+    private void Awake()
+    {
+        enemySprite = GetComponent<SpriteRenderer>();
+        enemySprite.sprite = enemyModel.Sprite;
+    }
+}
---
+++ Exercício1/Exercício1/Assets/Scripts/EnemyModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EnemyModel")]
+public class EnemyModel : ScriptableObject
+{
+
+    [SerializeField]private Sprite _sprite;
+    public Sprite Sprite => _sprite;
+}
